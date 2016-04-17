e733dd10532e645a2588f9fb03c290adf1c5c1b3
maked0n <alex.mikhalevich@gmail.com>
2016-04-17 23:56:33 +0300

    Minor changes

--- iobject3d.h
+++ iobject3d.h
@@ -5,5 +5,7 @@ class IObject3D {
 		virtual bool intersect(const CVector3D& ray_vector, CPoint3D& intersection) = 0;
 		virtual CColor get_intersection_color(const CPoint3D& intersection) = 0;
 		virtual CVector3D get_normal_vector(const CPoint3D& intersection) = 0;
+		virtual CPoint3D get_min_boundary_point() = 0;
+		virtual CPoint3D get_max_boundary_point() = 0;
 		virtual ~IObject3D() {}
 };
