211ad3ad6b13238c9dcfa73ced76084c551e131f
maked0n <alex.mikhalevich@gmail.com>
2016-04-23 16:57:14 +0300

    Command-line arguments supporting added

--- main.cpp
+++ main.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
-#include "csphere.h"
+#include <string>
+#include "cscene.h"
+#include "cparser.h"
 
-int main() {
-	CSphere sphere;
-	CVector3D v(CPoint3D(), CPoint3D(5.0, 5.0, 5.0));
-	CPoint3D p;
-	if(sphere.intersect(v, p)) std::cout << "yes: (" << p.get_x() << ", " << p.get_y() << ", " << p.get_z() << ")" << std::endl;
-	else std::cout << "no";
+int main(int argc, char** argv) {
+	bool gpu_process = false;
+	bool testing = false;
+	int width = 800;
+	int height = 800;
+	std::string filename = "";
+
+	if(argc > 1) {
+		for(int i = 0; i < argc; ++i) {
+			if(strcmp(argv[i], "-g") == 0 || strcmp(argv[i], "--gpu") == 0)
+				gpu_process = true;
+			else if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--height") == 0) {
+				height = atoi(argv[i + 1]);
+				++i;
+			}
+			else if(strcmp(argv[i], "-w") == 0 || strcmp(argv[i], "--width") == 0) {
+				height = atoi(argv[i + 1]);
+				++i;
+			}
+			else if(strcmp(argv[i], "--fullscreen") == 0) {
+				//TODO: fullscreen
+			}				
+			else if(strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--enable-testing") == 0)
+				testing = true;
+			else if(strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--file") == 0) {
+				filename = std::string(argv[i + 1]);
+				++i;
+			}
+
+		}
+	}
+
+	if(filename == "") {
+		std::cerr << "Filename not specified! Use -f or --file to open scene file." << std::endl;
+		exit(0);
+	}
+
+	CScene scene(width, height);
+	CCustomParser parser;
+	scene.load_file(&parser, filename);
+	scene.render(gpu_process, testing);
+
 	return 0;
 }
