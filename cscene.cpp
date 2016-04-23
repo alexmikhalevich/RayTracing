548053ad54fb7aed7a3f7386ea122da7ca9046bb
maked0n <alex.mikhalevich@gmail.com>
2016-04-23 16:50:36 +0300

    Scene representation

--- cscene.cpp
+++ cscene.cpp
@@ -1,7 +1,22 @@
 #include "cscene.h"
 
-void CScene::read(std::string filename) {
+void CScene::load_file(IParser* parser, const std::string& filename) {
+	parser->parse(filename);
+	m_objects = parser->get_objects();
+	m_camera = parser->get_camera();
+	/*for(IObject3D* obj : res) {
+		m_objects.push_back(obj);
+	}*/
 }
 
-void CScene::render() const {
+void CScene::render(bool gpu_process, bool testing) {
+	if(!gpu_process) {
+		CKDTreeCPU kdtree(m_objects);
+		CRenderer renderer(m_camera, &kdtree, m_width, m_height);
+		renderer.render();
+	}
+}
+
+void CScene::load_object(IObject3D* object) {
+	m_objects.push_back(object);
 }
--- cscene.h
+++ cscene.h
@@ -1,13 +1,25 @@
+#ifndef CSCENE_H
+#define CSCENE_H
 #include <string>
 #include <vector>
 #include "csphere.h"
+#include "cparser.h"
+#include "crenderer.h"
+#include "ckdtreecpu.h"
+#include "ccamera.hpp"
+#include "iparser.h"
 
 class CScene {
 	private:
 		std::vector<IObject3D*> m_objects;
+		CCamera m_camera;
+		int m_width;
+		int m_height;
 	public:
-		CScene() {}
+		CScene(int w, int h) : m_width(w), m_height(h) {}
 		CScene(const CScene& s) = delete;
-		void read(std::string filename);
-		void render() const;
+		void load_file(IParser* parser, const std::string& filename);
+		void load_object(IObject3D* object);
+		void render(bool gpu_process, bool testing);
 };
+#endif //CSCENE_H
