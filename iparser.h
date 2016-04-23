d1453c5f72905383d8679ebdd96832541e8a4ff1
maked0n <alex.mikhalevich@gmail.com>
2016-04-23 16:52:34 +0300

    The parser interface

---
+++ iparser.h
@@ -0,0 +1,13 @@
+#ifndef IPARSER_H
+#define IPARSER_H
+#include "iobject3d.h"
+#include "ccamera.hpp"
+
+class IParser {
+	public:
+		virtual void parse(const std::string& filename) = 0;
+		virtual std::vector<IObject3D*>& get_objects() = 0;
+		virtual CCamera get_camera() const = 0; 
+		virtual ~IParser() {}
+};
+#endif //IPARSER_H
