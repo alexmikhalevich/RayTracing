24454830709bd35d44ea858eb5c60b5eb0af4a62
maked0n <alex.mikhalevich@gmail.com>
2016-04-30 16:11:05 +0300

    Lighter implementation

---
+++ clighter.hpp
@@ -0,0 +1,25 @@
+#ifndef CLIGHTER_HPP
+#define CLIGHTER_HPP
+#include "geometry.hpp"
+
+const double SHADOW_INTENSITY = 0.1;
+
+class CLighter {
+	private:
+		CPoint3D m_position;
+		double m_intensity;
+	public:
+		CLighter(const CPoint3D& p, double i = 1) : m_position(p), m_intensity(i) {
+			if(m_intensity > 1) m_intensity = 1;
+			if(m_intensity < 0) m_intensity = 0;
+		}
+
+		double get_intensity(const CPoint3D& p) const { 
+			return m_intensity; 
+		}
+		void set_intensity(double intensity) { m_intensity = intensity; }
+
+		CPoint3D get_position() const { return m_position; }
+		void set_position(const CPoint3D& position) { m_position = position; }
+};
+#endif //CLIGHTER_HPP
