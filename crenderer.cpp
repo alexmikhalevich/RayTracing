7b14178bab39c0511dca8cae2497b6bbdb54a25a
maked0n <alex.mikhalevich@gmail.com>
2016-04-30 22:49:51 +0300

    Support fot light sources added

--- crenderer.cpp
+++ crenderer.cpp
@@ -6,42 +6,55 @@ CRenderer::~CRenderer() {
 	SDL_Quit();
 }
 
-CVector3D CRenderer::GetRay(const CPoint3D& screen_point) {
+CVector3D CRenderer::GetRay(const CPoint3D& screen_point) const {
 	CPoint3D plane_point;
-
-	//FIXME: x-coordinate is set incorrectly: plane is located behind the camera
-	plane_point.set_x(PROJ_PLANE_DIST + m_camera.get_position().get_x());
-	plane_point.set_y(screen_point.get_x() - m_width / 2);
-	plane_point.set_z(m_height / 2 - screen_point.get_y());
+	plane_point.set_x(m_camera.get_position().get_x() 
+			+ m_camera.get_view().get_coordinates().get_x() * PROJ_PLANE_DIST / m_camera.get_view().length());
+	plane_point.set_y((screen_point.get_x() - m_width / 2) / 300);	//TODO: replace this constant
+	plane_point.set_z((screen_point.get_y() - m_height / 2) / 300);
 
 	CVector3D ray_vector(m_camera.get_position(), plane_point);
-	return m_rot_matrix * ray_vector;
+	return ray_vector; 
 }
 
-void CRenderer::FindRotMatrix() {
-	CVector3D cam_basis_x = m_camera.get_view();
-	cam_basis_x.normalize();
-	CVector3D cam_basis_y(CPoint3D(0, 0, 0), CPoint3D(-cam_basis_x.get_coordinates().get_y() / cam_basis_x.get_coordinates().get_x(), 1, 0));
-	cam_basis_y.normalize();
-	CVector3D cam_basis_z(CPoint3D(0, 0, 0), CPoint3D(-cam_basis_x.get_coordinates().get_z() / cam_basis_x.get_coordinates().get_x(), 0, 1));
-	cam_basis_z.normalize();
-	double cx_x = cam_basis_x.get_coordinates().get_x();
-	double cx_y = cam_basis_x.get_coordinates().get_y();
-	double cx_z = cam_basis_x.get_coordinates().get_z();
-	double cy_x = cam_basis_y.get_coordinates().get_x();
-	double cy_y = cam_basis_y.get_coordinates().get_y();
-	double cy_z = cam_basis_y.get_coordinates().get_z();
-	double cz_x = cam_basis_z.get_coordinates().get_x();
-	double cz_y = cam_basis_z.get_coordinates().get_y();
-	double cz_z = cam_basis_z.get_coordinates().get_z();
-	CMatrix3D rot_matrix(cx_x, cy_x, cz_x, cx_y, cy_y, cz_y, cx_z, cy_z, cz_z);
-	rot_matrix.inverse();
-	m_rot_matrix = rot_matrix;
+inline double CRenderer::GetIntencity(const CVector3D& normal, const CLighter& lighter, const CPoint3D& intersection) const {
+	CVector3D ray(m_camera.get_position(), intersection);
+	double result = lighter.get_intensity(intersection);
+	double distance = lighter.get_position().get_distance(intersection);
+	result /= (distance * distance);
+	double cos_angle = (normal.get_coordinates().get_x() * ray.get_coordinates().get_x()
+		+ normal.get_coordinates().get_y() * ray.get_coordinates().get_y()
+		+ normal.get_coordinates().get_z() * ray.get_coordinates().get_z()) / ray.length();
+	result *= std::abs(cos_angle);
+	result *= INTENSITY_COEFF;
+	return result;
 }
 
-void CRenderer::render(bool testing) {
+inline CColor CRenderer::RayTracing(double backlight, const std::vector<CLighter>& lighters, const CVector3D& ray, const CColor& background) const {
+	CColor res_color, intersection_color;
+	IObject3D* obj = NULL;
+	CPoint3D intersection, tmp;
+	if(lighters.empty()) return background;
+
+	if(m_tree->find_intersection(ray, obj, intersection)) {
+		intersection_color = obj->get_intersection_color(intersection);
+		for(CLighter lighter : lighters) {
+			if(m_tree->find_intersection(CVector3D(intersection, lighter.get_position()), obj, tmp)) {
+				intersection_color *= backlight / 100;
+				res_color += intersection_color;
+			}
+			else { 
+				intersection_color *= GetIntencity(obj->get_normal_vector(intersection), lighter, intersection);
+				res_color += intersection_color;
+			}
+		}
+		return res_color;
+	}
+	else return background;
+}
+
+void CRenderer::render(double backlight, const std::vector<CLighter>& lighters, bool testing) {
 	std::chrono::steady_clock::time_point t1;
-	FindRotMatrix();
 	SDL_Event event;
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_CreateWindowAndRenderer(m_width, m_height, 0, &window, &renderer);
@@ -51,15 +64,9 @@ void CRenderer::render(bool testing) {
 	if(testing) t1 = std::chrono::steady_clock::now();
 	for(int x = 0; x < m_width; ++x) {
 		for(int y = 0; y < m_height; ++y) {
-			IObject3D* obj = NULL;
-			CPoint3D intersection;
 			CVector3D vect = GetRay(CPoint3D(x, y, 0));
-			if(m_tree->find_intersection(vect, obj, intersection)) {
-				CColor c = obj->get_intersection_color(intersection);
-				SDL_SetRenderDrawColor(renderer, c.get_red(), c.get_green(), c.get_blue(), 0); 
-			}
-			else 
-				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0); 
+			CColor c = RayTracing(backlight, lighters, vect, CColor(0, 0, 0));
+			SDL_SetRenderDrawColor(renderer, c.get_red(), c.get_green(), c.get_blue(), 0); 
 			SDL_RenderDrawPoint(renderer, x, y);
 		}
 	}
--- crenderer.h
+++ crenderer.h
@@ -4,8 +4,10 @@
 #include <chrono>
 #include "ikdtree.h"
 #include "ccamera.hpp"
+#include "clighter.hpp"
 
-const double PROJ_PLANE_DIST = 1;
+const double PROJ_PLANE_DIST = 1.0;
+const double INTENSITY_COEFF = 10.0;
 
 class CRenderer {
 	private:
@@ -15,13 +17,13 @@ class CRenderer {
 		int m_height;
 		SDL_Renderer* renderer;
 		SDL_Window* window;
-		CMatrix3D m_rot_matrix;
-		CVector3D GetRay(const CPoint3D& screen_point);
-		void FindRotMatrix();
+		CVector3D GetRay(const CPoint3D& screen_point) const;
+		inline CColor RayTracing(double backlight, const std::vector<CLighter>& lighters, const CVector3D& ray, const CColor& background) const;
+		inline double GetIntencity(const CVector3D& normal, const CLighter& l, const CPoint3D& intersection) const;
 	public:
 		CRenderer() = delete;
 		CRenderer(const CCamera& c, IKDTree* t, int w = -1, int h = -1) : m_camera(c), m_tree(t), m_width(w), m_height(h) {}
 		~CRenderer();
-		void render(bool testing);
+		void render(double backlight, const std::vector<CLighter>& lighters, bool testing);
 };
 #endif //CRENDERER_H
