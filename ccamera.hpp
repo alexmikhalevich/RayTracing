473ec79eeaa6c7576a0b1df3480e26d19be079b1
maked0n <alex.mikhalevich@gmail.com>
2016-04-30 16:14:30 +0300

    Camera basis normalization added

--- ccamera.hpp
+++ ccamera.hpp
@@ -8,12 +8,19 @@ class CCamera {
 		CVector3D m_view;
 	public:
 		CCamera() : m_position(CPoint3D()), m_view(CVector3D()) {}
-		CCamera(const CPoint3D& p, const CVector3D& v) : m_position(p), m_view(v) {}
-		CCamera(const CPoint3D& p, const CPoint3D& v) : m_position(p), m_view(CVector3D(p, v)) {}
+		CCamera(const CPoint3D& p, const CVector3D& v) : m_position(p), m_view(v) {
+			m_view.normalize();
+		}
+		CCamera(const CPoint3D& p, const CPoint3D& v) : m_position(p), m_view(CVector3D(p, v)) {
+			m_view.normalize();
+		}
 
 		CPoint3D get_position() const { return m_position; }
 		CVector3D get_view() const { return m_view; }
 		void set_position(const CPoint3D& p) { m_position = p; }
-		void set_view(const CVector3D& v) { m_view = v; }
+		void set_view(const CVector3D& v) { 
+			m_view = v; 
+			m_view.normalize();
+		}
 };
 #endif //CCAMERA_HPP
