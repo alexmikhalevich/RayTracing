4c656bfc2dfcb428557f160f691a122b6d314821
maked0n <alex.mikhalevich@gmail.com>
2016-05-26 00:29:32 +0300

    Material implementation

---
+++ cmaterial.hpp
@@ -0,0 +1,50 @@
+#include "geometry.hpp"
+
+class CMaterial {
+	private:
+		CColor m_color;
+		double m_alpha;
+		double m_reflect;
+		double m_refract;
+	public:
+		CMaterial() : m_color(CColor()), m_alpha(1.0), m_reflect(0), m_refract(0) {}
+		CMaterial(const CColor& col, double a, double refl, double refr) {
+			m_color = col;
+			m_alpha = std::min(std::max(0, a), 1);
+			m_reflect = std::min(std::max(refl, 0), 1);
+			m_refract = std::max(0, refr);
+		}
+		
+		CColor get_color() const {
+			return m_color;
+		}
+
+		double get_alpha() const {
+			return m_alpha;
+		}
+
+		double get_reflect() const {
+			return m_reflect;
+		}
+
+		double get_refract() const {
+			return m_refract;
+		}
+
+		void set_color(const CColor& col) {
+			m_color = col;
+		}
+
+		void set_alpha(double a) {
+			m_alpha = a;
+		}
+
+		void set_reflection(double r) {
+			m_reflect = r;
+		}
+
+		void set_refraction(double r) {
+			m_refract = r;
+		}
+};
+
