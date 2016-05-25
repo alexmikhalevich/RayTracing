c5990b610d203abdd13e16200e062f45232b1a8b
maked0n <alex.mikhalevich@gmail.com>
2016-05-26 00:31:13 +0300

    Exceptions

---
+++ cexception.hpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class IException {
+	public:
+		virtual void what() const = 0;
+		virtual ~IException() = 0;
+};
+
+class ExEOF : public IException {
+	public:
+		virtual void what() const { std::cerr << "[EE]: Unexpected end of file" << std::endl; }
+};
+
+class ExInvalidLighter : public IException {
+	public:
+		virtual void what() const { std::cerr << "[EE]: Invalid lighter format" << std::endl; }
+};
+
+class ExInvalidSphere : public IException {
+	public:
+		virtual void what() const { std::cerr << "[EE]: Invalid sphere format" << std::endl; }
+};
+
+class ExInvalidTriangle : public IException {
+	public:
+		virtual void what() const { std::cerr << "[EE]: Invalid triangle format" << std::endl; }
+};
+
+class ExInvalidQuadrangle : public IException {
+	public:
+		virtual void what() const { std::cerr << "[EE]: Invalid quadrangle format" << std::endl; }
+};
